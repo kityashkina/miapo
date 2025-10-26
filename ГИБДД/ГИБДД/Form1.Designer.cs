4d9135a9eae13e4679345da2b2b99a3b7328fea7
Екатерина К <ekat66215@gmail.com>
2025-10-26 17:14:49 +0300

    Добавьте файлы проекта.

---
+++ ГИБДД/ГИБДД/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ГИБДД
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Rubik One", 47.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(276, 76);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(281, 80);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "ГИБДД";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::ГИБДД.Properties.Resources.image_1;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(270, 222);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Rubik One", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(553, 263);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(144, 45);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Логин";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Rubik One", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(535, 385);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(171, 45);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Пароль";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(493, 326);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(254, 31);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.Text = "Введите логин";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox2.Location = new System.Drawing.Point(493, 449);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(254, 31);
+			this.textBox2.TabIndex = 5;
+			this.textBox2.Text = "Введите пароль";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.LightSkyBlue;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.Font = new System.Drawing.Font("Rubik One", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(450, 533);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(325, 69);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Войти";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(356, 25);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(596, 228);
+			this.panel1.TabIndex = 7;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(1264, 793);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ ГИБДД/ГИБДД/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ГИБДД
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		public class RoundBtn : Control
+		{
+			public RoundBtn()
+			{
+
+			}
+		}
+
+		private void CenterPanelWithOffset()
+		{
+			panel1.Location = new Point((this.ClientSize.Width - panel1.Width) / 2, 25);
+			label2.Location = new Point((this.ClientSize.Width - label2.Width) / 2, label2.Location.Y);
+			label3.Location = new Point((this.ClientSize.Width - label3.Width) / 2, label3.Location.Y);
+			textBox1.Location = new Point((this.ClientSize.Width - textBox1.Width) / 2, textBox1.Location.Y);
+			textBox2.Location = new Point((this.ClientSize.Width - textBox2.Width) / 2, textBox2.Location.Y);
+			button1.Location = new Point((this.ClientSize.Width - button1.Width) / 2, button1.Location.Y);
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			CenterPanelWithOffset();
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
